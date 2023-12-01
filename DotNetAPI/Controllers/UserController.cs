07f1a85dedada8e0a76b3cdf7cbfc8623e6fd7bf
Tajinder Singh Dhoot <tajindersinghdhoot@Singh-On-Air.local>
2023-12-01 13:38:46 -0500

    added models, controllers to get, update and add a user

---
+++ DotNetAPI/Controllers/UserController.cs
@@ -0,0 +1,106 @@
+using Microsoft.AspNetCore.Mvc;
+using DotNetAPI.Models;
+using DotNetAPI.Data;
+namespace DotNetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+
+    DataContextDapper _dapper;
+
+    public UserController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+    }
+
+    // [HttpGet("GetUsers")]
+    // public string GetUsers()
+    // {
+    //     return "123";
+    // }
+    
+    
+    [HttpGet("GetUsers")]
+    public IEnumerable<User> GetUsers()
+    {
+        string sql = @"
+            SELECT  [UserId],
+                    [FirstName],
+                    [LastName],
+                    [Email],
+                    [Gender],
+                    [Active]
+            FROM  TutorialAppSchema.Users";
+        IEnumerable<User> users = _dapper.LoadData<User>(sql);
+        return users;
+    }
+
+    [HttpGet("GetSingleUser/{userId}")]
+    public User GetSingleUser(int userId)
+    {
+        string sql = @"
+            SELECT  [UserId],
+                    [FirstName],
+                    [LastName],
+                    [Email],
+                    [Gender],
+                    [Active]
+            FROM  TutorialAppSchema.Users
+            WHERE UserID = " + userId.ToString();
+
+        User singleUser = _dapper.LoadDataSingle<User>(sql);
+        return singleUser;
+    }
+
+    [HttpPut("EditUser")]
+    public IActionResult EditUser(User user)
+    {
+        string sql = @"
+            UPDATE TutorialAppSchema.Users
+            SET [FirstName] = '" + user.FirstName +
+                "', [LastName] = '" + user.LastName +
+                "', [Email] = '" + user.Email +
+                "', [Gender] = '" + user.Gender +
+                "', [Active] = '" + user.Active +
+            "' WHERE [UserId] = " + user.UserId;
+
+        Console.WriteLine(sql);
+
+        if(_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+
+        throw new Exception("Failed to update user");
+    }
+
+    [HttpPost("AddUser")]
+    public IActionResult AddUser(User user)
+    {
+        string sql = @"
+            INSERT INTO TutorialAppSchema.Users(
+                [FirstName],
+                [LastName],
+                [Gender],
+                [Email],
+                [Active]
+            ) VALUES (" +
+                "'" + user.FirstName +
+                "', '" + user.LastName +
+                "', '" + user.Gender +
+                "', '" + user.Email +
+                "', '" + user.Active +
+            "')";
+
+        Console.WriteLine("Add User sql Query:" +sql);
+
+        if(_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+
+        throw new Exception("Failed to add new user");
+    }
+}
