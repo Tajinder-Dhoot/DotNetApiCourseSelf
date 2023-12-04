b24c3a1142299759236376f4c2cb3e0b14ddb588
Tajinder Singh Dhoot <tajindersinghdhoot@Singh-On-Air.local>
2023-12-03 22:56:22 -0500

    register new user

--- DotNetAPI/Controllers/AuthController.cs
+++ DotNetAPI/Controllers/AuthController.cs
@@ -1,6 +1,12 @@
 
-using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
 using DotNetAPI.Data;
+using DotNetAPI.Dtos;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace DotNetAPI.Controllers
 {
@@ -13,6 +19,89 @@ namespace DotNetAPI.Controllers
         {
             _config = config;
             _dapper = new DataContextDapper(config);
+        }
+
+        [HttpPost("Register")]
+        public IActionResult Register(UserForRegistrationDto userForRegistration)
+        {
+            if (userForRegistration.Password == userForRegistration.PasswordConfirm)
+            {
+                string sqlCheckUserExists = "SELECT Email FROM TutorialAppSchema.Auth WHERE Email = '" 
+                    + userForRegistration.Email + "'";
+
+                IEnumerable<string> existingUsers = _dapper.LoadData<string>(sqlCheckUserExists);
+                if(existingUsers.Count() == 0)
+                {
+                    byte[] passwordSalt = new byte[128/ 8];
+                    using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+                    {
+                        rng.GetNonZeroBytes(passwordSalt);
+                    }
+
+                    byte[] passwordHash = GetPasswordHash(userForRegistration.Password, passwordSalt);
+
+                    string sqlAddAuth = @"
+                        INSERT INTO TutorialAppSchema.Auth  ([Email],
+                        [PasswordHash],
+                        [PasswordSalt]) VALUES ('" + userForRegistration.Email +
+                        "', @PasswordHash, @PasswordSalt)";
+
+                    List<SqlParameter> sqlparameters = [];
+
+                    SqlParameter passwordSaltParameter = new("@PasswordSalt", SqlDbType.VarBinary)
+                    {
+                        Value = passwordSalt
+                    };
+
+                    SqlParameter passwordHashParameter = new("@PasswordHash", SqlDbType.VarBinary)
+                    {
+                        Value = passwordHash
+                    };
+
+                    sqlparameters.Add(passwordSaltParameter);
+                    sqlparameters.Add(passwordHashParameter);
+
+                    if(_dapper.ExecuteSqlWithParameters(sqlAddAuth, sqlparameters))
+                    {
+                        string sqlAddUser = @"
+                            INSERT INTO TutorialAppSchema.Users(
+                                [FirstName],
+                                [LastName],
+                                [Email],
+                                [Gender],
+                                [Active]
+                            ) VALUES (" +
+                                "'" + userForRegistration.FirstName + 
+                                "', '" + userForRegistration.LastName +
+                                "', '" + userForRegistration.Email + 
+                                "', '" + userForRegistration.Gender + 
+                                "', 1)";
+
+                        if(_dapper.ExecuteSql(sqlAddUser))
+                        {
+                            return Ok();
+                        }
+                        throw new Exception("failed to Add user!");
+                    }
+                    throw new Exception("Failed to Register User!");
+                }
+                throw new Exception("User already exists with the email provided!");
+            }
+            throw new Exception("Passwords do not match!");
+        }
+
+        private byte[] GetPasswordHash(string password, byte[] passwordSalt)
+        {
+            string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey").Value +
+                Convert.ToBase64String(passwordSalt);
+
+            return KeyDerivation.Pbkdf2(
+                password: password,
+                salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 1000000,
+                numBytesRequested: 256 / 8
+            );
         }
     }
 }
